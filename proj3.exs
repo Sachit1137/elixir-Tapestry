82ed527cb0ab74524fee2ecb34a4da38e223a386
Sachit1137 <55988870+Sachit1137@users.noreply.github.com>
2019-10-19 02:23:56 -0400

    Update proj3.exs

--- proj3.exs
+++ proj3.exs
@@ -18,9 +18,40 @@ defmodule Proj3 do
 
 
     # Indexing all the PID's with the hexadecimal node ID's
+    # indexed_actors = Stream.with_index(allNodes,1) |> Enum.reduce(%{}, fn {pids, nodeID}, acc -> Map.put(acc, String.slice(:crypto.hash(:sha, "#{nodeID}") |> Base.encode16 , 1..4) , pids) end)
     indexed_actors = Stream.with_index(allNodes,1) |> Enum.reduce(%{}, fn {pids, nodeID}, acc -> Map.put(acc, :crypto.hash(:sha, "#{nodeID}") |> Base.encode16 , pids) end)
-    IO.inspect indexed_actors
+    # IO.inspect indexed_actors
 
+    routing_tables = Map.new()
+
+    list_of_hexValues = for {hash_key , _pid} <- indexed_actors do
+      String.to_charlist(hash_key)
+    end
+    IO.inspect list_of_hexValues
+    # IO.inspect ['56A1','5DE6', '56B9']
+
+    # fill_routing_table('56A1',['5DE6', '56B9'])
+
+
+    for {hash_key , _pid} <- indexed_actors do
+      # list_of_hexValues -- [String.to_charlist(hash_key)]
+      IO.puts "key changed"
+      IO.inspect fill_routing_table(String.to_charlist(hash_key), list_of_hexValues -- [String.to_charlist(hash_key)])
+    end
+    # IO.inspect x
+  end
+
+  def fill_routing_table(hash_key,list_of_neighbors) do
+
+    Enum.reduce(list_of_neighbors, %{}, fn neighbor_key,acc->
+
+      level = 0
+      key = Enum.reduce_while(neighbor_key, 0 ,fn char2,level ->
+        if Enum.at(hash_key,level) == char2, do: {:cont, level+1}, else: {:halt, {level,List.to_string([char2])}}
+      end)
+
+      Map.put(acc, key, neighbor_key)
+    end)
   end
 
   def init(:ok) do
