ef738a265c6d37425bb794d24b76ce76769e2aeb
Smrati8 <56731723+Smrati8@users.noreply.github.com>
2019-10-20 02:16:53 -0400

    Full Table state added

--- proj3.exs
+++ proj3.exs
@@ -6,8 +6,6 @@ defmodule Proj3 do
     [numNodes, numRequests] = input
     numNodes = numNodes |> String.to_integer()
     numRequests = numRequests |> String.to_integer()
-    # IO.puts(numNodes)
-    # IO.puts(numRequests)
 
     allNodes =
       Enum.map(1..numNodes, fn x ->
@@ -16,355 +14,28 @@ defmodule Proj3 do
         pid
       end)
 
-    #IO.inspect(allNodes)
-
     indexed_actors =
       Stream.with_index(allNodes, 1)
       |> Enum.reduce(%{}, fn {pids, nodeID}, acc ->
         Map.put(acc, :crypto.hash(:sha, "#{nodeID}") |> Base.encode16(), pids)
       end)
 
-    #IO.inspect(indexed_actors)
+    IO.inspect(indexed_actors)
 
-    neighbors = set_neighbours(numNodes,indexed_actors)
-    #IO.inspect(neighbors)
-  end
-
-  def set_neighbours(numNodes,indexed_actors) do
-    rowCount = :math.pow(numNodes, 1 / 3) |> ceil
-
-    # Creating a map for indexing all Torus nodes
-    tupleList =
-      Enum.map(1..rowCount, fn x ->
-        Enum.map(1..rowCount, fn y -> Enum.map(1..rowCount, fn z -> {x, y, z} end) end)
-      end)
-      |> List.flatten()
-
-    # Associating the map indexed values with the Nodes
-    torusMap =
-      Stream.with_index(tupleList, 1)
-      |> Enum.reduce(%{}, fn {tupleValue, nodes}, acc -> Map.put(acc, tupleValue, nodes) end)
-
-    # Forming a key value pair of Nodes and the index
-    torusMapNew =
-      Stream.with_index(tupleList, 1)
-      |> Enum.reduce(%{}, fn {nodes, tupleValue}, acc -> Map.put(acc, tupleValue, nodes) end)
+    list_of_hexValues =
+      for {hash_key, _pid} <- indexed_actors do
+        String.to_charlist(hash_key)
+      end
 
     Enum.map(1..numNodes, fn x ->
-      # Assigning the current node to the map
-      {a, b, c} = Map.fetch!(torusMapNew, x)
-
-      # Calculating the neighbors
-      neighbors =
-        cond do
-          a == 1 ->
-            cond do
-              b == 1 && c == 1 ->
-                [
-                  Map.fetch!(torusMap, {a + 1, b, c}),
-                  Map.fetch!(torusMap, {a, b + 1, c}),
-                  Map.fetch!(torusMap, {a, b, c + 1}),
-                  Map.fetch!(torusMap, {a, b, c + rowCount - 1}),
-                  Map.fetch!(torusMap, {a, b + rowCount - 1, c}),
-                  Map.fetch!(torusMap, {a + rowCount - 1, b, c})
-                ]
-
-              b == 1 && (c > 1 && c < rowCount) ->
-                [
-                  Map.fetch!(torusMap, {a, b, c - 1}),
-                  Map.fetch!(torusMap, {a, b, c + 1}),
-                  Map.fetch!(torusMap, {a, b + 1, c}),
-                  Map.fetch!(torusMap, {a + 1, b, c}),
-                  Map.fetch!(torusMap, {a, b + rowCount - 1, c}),
-                  Map.fetch!(torusMap, {a + rowCount - 1, b, c})
-                ]
-
-              b == 1 && c == rowCount ->
-                [
-                  Map.fetch!(torusMap, {a, b, c - rowCount + 1}),
-                  Map.fetch!(torusMap, {a, b, c - 1}),
-                  Map.fetch!(torusMap, {a, b + 1, c}),
-                  Map.fetch!(torusMap, {a + 1, b, c}),
-                  Map.fetch!(torusMap, {a, b + rowCount - 1, c}),
-                  Map.fetch!(torusMap, {a + rowCount - 1, b, c})
-                ]
-
-              b > 1 && b < rowCount && c == 1 ->
-                [
-                  Map.fetch!(torusMap, {a, b - 1, c}),
-                  Map.fetch!(torusMap, {a, b + 1, c}),
-                  Map.fetch!(torusMap, {a, b, c + 1}),
-                  Map.fetch!(torusMap, {a + 1, b, c}),
-                  Map.fetch!(torusMap, {a, b, c + rowCount - 1}),
-                  Map.fetch!(torusMap, {a + rowCount - 1, b, c})
-                ]
-
-              b > 1 && b < rowCount && c == rowCount ->
-                [
-                  Map.fetch!(torusMap, {a, b - 1, c}),
-                  Map.fetch!(torusMap, {a, b + 1, c}),
-                  Map.fetch!(torusMap, {a, b, c - 1}),
-                  Map.fetch!(torusMap, {a + 1, b, c}),
-                  Map.fetch!(torusMap, {a, b, c - rowCount + 1}),
-                  Map.fetch!(torusMap, {a + rowCount - 1, b, c})
-                ]
-
-              b == rowCount && c == 1 ->
-                [
-                  Map.fetch!(torusMap, {a, b - 1, c}),
-                  Map.fetch!(torusMap, {a, b, c + 1}),
-                  Map.fetch!(torusMap, {a + 1, b, c}),
-                  Map.fetch!(torusMap, {a, b - rowCount + 1, c}),
-                  Map.fetch!(torusMap, {a, b, c + rowCount - 1}),
-                  Map.fetch!(torusMap, {a + rowCount - 1, b, c})
-                ]
-
-              b == rowCount && c > 1 && c < rowCount ->
-                [
-                  Map.fetch!(torusMap, {a, b, c + 1}),
-                  Map.fetch!(torusMap, {a, b, c - 1}),
-                  Map.fetch!(torusMap, {a, b - rowCount + 1, c}),
-                  Map.fetch!(torusMap, {a + rowCount - 1, b, c}),
-                  Map.fetch!(torusMap, {a, b - 1, c}),
-                  Map.fetch!(torusMap, {a + 1, b, c})
-                ]
-
-              b == rowCount && c == rowCount ->
-                [
-                  Map.fetch!(torusMap, {a + 1, b, c}),
-                  Map.fetch!(torusMap, {a, b - 1, c}),
-                  Map.fetch!(torusMap, {a, b, c - 1}),
-                  Map.fetch!(torusMap, {a + rowCount - 1, b, c}),
-                  Map.fetch!(torusMap, {a, b - rowCount + 1, c}),
-                  Map.fetch!(torusMap, {a, b, c - rowCount + 1})
-                ]
-
-              b > 1 && b < rowCount && c > 1 && c < rowCount ->
-                [
-                  Map.fetch!(torusMap, {a, b + 1, c}),
-                  Map.fetch!(torusMap, {a, b - 1, c}),
-                  Map.fetch!(torusMap, {a, b, c + 1}),
-                  Map.fetch!(torusMap, {a, b, c - 1}),
-                  Map.fetch!(torusMap, {a + 1, b, c}),
-                  Map.fetch!(torusMap, {a + rowCount - 1, b, c})
-                ]
-
-              true ->
-                []
-            end
-
-          a == rowCount ->
-            cond do
-              b == 1 && c == 1 ->
-                [
-                  Map.fetch!(torusMap, {a - 1, b, c}),
-                  Map.fetch!(torusMap, {a, b, c + 1}),
-                  Map.fetch!(torusMap, {a, b + 1, c}),
-                  Map.fetch!(torusMap, {a - rowCount + 1, b, c}),
-                  Map.fetch!(torusMap, {a, b + rowCount - 1, c}),
-                  Map.fetch!(torusMap, {a, b, c + rowCount - 1})
-                ]
-
-              b == 1 && (c > 1 && c < rowCount) ->
-                [
-                  Map.fetch!(torusMap, {a, b, c + 1}),
-                  Map.fetch!(torusMap, {a, b, c - 1}),
-                  Map.fetch!(torusMap, {a - 1, b, c}),
-                  Map.fetch!(torusMap, {a, b + 1, c}),
-                  Map.fetch!(torusMap, {a - rowCount + 1, b, c}),
-                  Map.fetch!(torusMap, {a, b + rowCount - 1, c})
-                ]
-
-              b == 1 && c == rowCount ->
-                [
-                  Map.fetch!(torusMap, {a - 1, b, c}),
-                  Map.fetch!(torusMap, {a, b + 1, c}),
-                  Map.fetch!(torusMap, {a, b, c - 1}),
-                  Map.fetch!(torusMap, {a, b, c - rowCount + 1}),
-                  Map.fetch!(torusMap, {a, b + rowCount - 1, c}),
-                  Map.fetch!(torusMap, {a - rowCount + 1, b, c})
-                ]
-
-              b > 1 && b < rowCount && c == 1 ->
-                [
-                  Map.fetch!(torusMap, {a, b + 1, c}),
-                  Map.fetch!(torusMap, {a, b - 1, c}),
-                  Map.fetch!(torusMap, {a, b, c + 1}),
-                  Map.fetch!(torusMap, {a - 1, b, c}),
-                  Map.fetch!(torusMap, {a - rowCount + 1, b, c}),
-                  Map.fetch!(torusMap, {a, b, c + rowCount - 1})
-                ]
-
-              b > 1 && b < rowCount && c == rowCount ->
-                [
-                  Map.fetch!(torusMap, {a, b + 1, c}),
-                  Map.fetch!(torusMap, {a, b - 1, c}),
-                  Map.fetch!(torusMap, {a, b, c - 1}),
-                  Map.fetch!(torusMap, {a - 1, b, c}),
-                  Map.fetch!(torusMap, {a - rowCount + 1, b, c}),
-                  Map.fetch!(torusMap, {a, b, c - rowCount + 1})
-                ]
-
-              b == rowCount && c == 1 ->
-                [
-                  Map.fetch!(torusMap, {a - 1, b, c}),
-                  Map.fetch!(torusMap, {a, b - 1, c}),
-                  Map.fetch!(torusMap, {a, b, c + 1}),
-                  Map.fetch!(torusMap, {a, b - rowCount + 1, c}),
-                  Map.fetch!(torusMap, {a, b, c + rowCount - 1}),
-                  Map.fetch!(torusMap, {a - rowCount + 1, b, c})
-                ]
-
-              b == rowCount && c > 1 && c < rowCount ->
-                [
-                  Map.fetch!(torusMap, {a, b, c - 1}),
-                  Map.fetch!(torusMap, {a, b, c + 1}),
-                  Map.fetch!(torusMap, {a, b - 1, c}),
-                  Map.fetch!(torusMap, {a - 1, b, c}),
-                  Map.fetch!(torusMap, {a - rowCount + 1, b, c}),
-                  Map.fetch!(torusMap, {a, b - rowCount + 1, c})
-                ]
-
-              b == rowCount && c == rowCount ->
-                [
-                  Map.fetch!(torusMap, {a - 1, b, c}),
-                  Map.fetch!(torusMap, {a, b - 1, c}),
-                  Map.fetch!(torusMap, {a, b, c - 1}),
-                  Map.fetch!(torusMap, {a, b, c - rowCount + 1}),
-                  Map.fetch!(torusMap, {a, b - rowCount + 1, c}),
-                  Map.fetch!(torusMap, {a - rowCount + 1, b, c})
-                ]
-
-              b > 1 && b < rowCount && c > 1 && c < rowCount ->
-                [
-                  Map.fetch!(torusMap, {a, b + 1, c}),
-                  Map.fetch!(torusMap, {a, b - 1, c}),
-                  Map.fetch!(torusMap, {a, b, c + 1}),
-                  Map.fetch!(torusMap, {a, b, c - 1}),
-                  Map.fetch!(torusMap, {a - 1, b, c}),
-                  Map.fetch!(torusMap, {a - rowCount + 1, b, c})
-                ]
-
-              true ->
-                []
-            end
-
-          b == 1 && a > 1 && a < rowCount ->
-            cond do
-              c == 1 ->
-                [
-                  Map.fetch!(torusMap, {a - 1, b, c}),
-                  Map.fetch!(torusMap, {a + 1, b, c}),
-                  Map.fetch!(torusMap, {a, b + 1, c}),
-                  Map.fetch!(torusMap, {a, b, c + 1}),
-                  Map.fetch!(torusMap, {a, b + rowCount - 1, c}),
-                  Map.fetch!(torusMap, {a, b, c + rowCount - 1})
-                ]
-
-              c == rowCount ->
-                [
-                  Map.fetch!(torusMap, {a - 1, b, c}),
-                  Map.fetch!(torusMap, {a + 1, b, c}),
-                  Map.fetch!(torusMap, {a, b + 1, c}),
-                  Map.fetch!(torusMap, {a, b, c - 1}),
-                  Map.fetch!(torusMap, {a, b, c - rowCount + 1}),
-                  Map.fetch!(torusMap, {a, b + rowCount - 1, c})
-                ]
-
-              c > 1 && c < rowCount ->
-                [
-                  Map.fetch!(torusMap, {a - 1, b, c}),
-                  Map.fetch!(torusMap, {a + 1, b, c}),
-                  Map.fetch!(torusMap, {a, b, c - 1}),
-                  Map.fetch!(torusMap, {a, b, c + 1}),
-                  Map.fetch!(torusMap, {a, b + 1, c}),
-                  Map.fetch!(torusMap, {a, b + rowCount - 1, c})
-                ]
-
-              true ->
-                []
-            end
-
-          b == rowCount && a > 1 && a < rowCount ->
-            cond do
-              c == 1 ->
-                [
-                  Map.fetch!(torusMap, {a + 1, b, c}),
-                  Map.fetch!(torusMap, {a - 1, b, c}),
-                  Map.fetch!(torusMap, {a, b - 1, c}),
-                  Map.fetch!(torusMap, {a, b, c + 1}),
-                  Map.fetch!(torusMap, {a, b, c + rowCount - 1}),
-                  Map.fetch!(torusMap, {a, b - rowCount + 1, c})
-                ]
-
-              c == rowCount ->
-                [
-                  Map.fetch!(torusMap, {a + 1, b, c}),
-                  Map.fetch!(torusMap, {a - 1, b, c}),
-                  Map.fetch!(torusMap, {a, b - 1, c}),
-                  Map.fetch!(torusMap, {a, b, c - 1}),
-                  Map.fetch!(torusMap, {a, b, c - rowCount + 1}),
-                  Map.fetch!(torusMap, {a, b - rowCount + 1, c})
-                ]
-
-              c > 1 && c < rowCount ->
-                [
-                  Map.fetch!(torusMap, {a + 1, b, c}),
-                  Map.fetch!(torusMap, {a - 1, b, c}),
-                  Map.fetch!(torusMap, {a, b, c + 1}),
-                  Map.fetch!(torusMap, {a, b, c - 1}),
-                  Map.fetch!(torusMap, {a, b - 1, c}),
-                  Map.fetch!(torusMap, {a, b - rowCount + 1, c})
-                ]
-
-              true ->
-                []
-            end
-
-          a > 1 && a < rowCount && b > 1 && b < rowCount && c == 1 ->
-            [
-              Map.fetch!(torusMap, {a + 1, b, c}),
-              Map.fetch!(torusMap, {a - 1, b, c}),
-              Map.fetch!(torusMap, {a, b + 1, c}),
-              Map.fetch!(torusMap, {a, b - 1, c}),
-              Map.fetch!(torusMap, {a, b, c + 1}),
-              Map.fetch!(torusMap, {a, b, c + rowCount - 1})
-            ]
-
-          a > 1 && a < rowCount && b > 1 && b < rowCount && c == rowCount ->
-            [
-              Map.fetch!(torusMap, {a + 1, b, c}),
-              Map.fetch!(torusMap, {a - 1, b, c}),
-              Map.fetch!(torusMap, {a, b + 1, c}),
-              Map.fetch!(torusMap, {a, b - 1, c}),
-              Map.fetch!(torusMap, {a, b, c - 1}),
-              Map.fetch!(torusMap, {a, b, c - rowCount + 1})
-            ]
-
-          a > 1 && a < rowCount && (b > 1 && b < rowCount) && (c > 1 && c < rowCount) ->
-            [
-              Map.fetch!(torusMap, {a - 1, b, c}),
-              Map.fetch!(torusMap, {a + 1, b, c}),
-              Map.fetch!(torusMap, {a, b - 1, c}),
-              Map.fetch!(torusMap, {a, b + 1, c}),
-              Map.fetch!(torusMap, {a, b, c + 1}),
-              Map.fetch!(torusMap, {a, b, c - 1})
-            ]
-
-          true ->
-            []
-        end
-
-      neighborHexVal =
-        Enum.map(neighbors, fn value ->
-          String.to_charlist(:crypto.hash(:sha, "#{value}") |> Base.encode16())
-        end)
-
       nodeID = :crypto.hash(:sha, "#{x}") |> Base.encode16()
 
       hash_key_routing_table =
-        fill_routing_table(String.to_charlist(nodeID), neighborHexVal)
-      GenServer.call(Map.fetch!(indexed_actors,nodeID), {:UpdateRoutingTable,hash_key_routing_table})
+        fill_routing_table(
+          String.to_charlist(nodeID),
+          list_of_hexValues -- [String.to_charlist(nodeID)]
+        )
+        GenServer.call(Map.fetch!(indexed_actors, nodeID), {:UpdateRoutingTable,hash_key_routing_table})
     end)
   end
 
@@ -408,10 +79,10 @@ defmodule Proj3 do
     GenServer.call(pid, {:updatePID, numNodeID})
   end
 
-  def handle_call({:UpdateRoutingTable,hash_key_routing_table},_from,state) do
+  def handle_call({:UpdateRoutingTable, hash_key_routing_table}, _from, state) do
     {nodeID, neighborTable, counter} = state
     state = {nodeID, hash_key_routing_table, counter}
-    IO.inspect state
+    IO.inspect(state)
     {:reply, neighborTable, state}
   end
 
