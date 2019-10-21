9752790ba12802c12da29ad93a5e5967a1e0e058
Sachit1137 <55988870+Sachit1137@users.noreply.github.com>
2019-10-21 01:57:28 -0400

    Hopping implemented using pids

--- proj3.exs
+++ proj3.exs
@@ -38,20 +38,21 @@ defmodule Proj3 do
         Map.put(all_routing_tables, hash_key, hash_key_routing_table)
       end)
 
-    # IO.inspect routing_tables
+    # IO.inspect(routing_tables)
 
     hopping_list =
-      Enum.reduce(indexed_actors, [], fn {source_ID, _pid}, final_hop_list ->
-        # IO.inspect source_ID
+      Enum.reduce(indexed_actors, [], fn {source_ID, pid}, final_hop_list ->
+        # IO.inspect(source_ID)
 
         destinationList = Enum.take_random(list_of_hexValues -- [source_ID], numRequests)
-        # IO.inspect destinationList
+        # IO.inspect(destinationList)
 
         source_routing_table = Map.fetch!(routing_tables, source_ID)
 
         final_hop_list ++
           implementing_tapestry(
             source_ID,
+            pid,
             destinationList,
             routing_tables,
             source_routing_table,
@@ -59,27 +60,36 @@ defmodule Proj3 do
           )
       end)
 
-    # IO.inspect (hopping_list)
+    # IO.inspect(hopping_list)
     max_hops = Enum.max(hopping_list)
     IO.puts("Maximum Hops = #{max_hops}")
   end
 
-  def implementing_tapestry(node_ID, destinationList, routing_tables, node_table, _indexed_actors) do
+  def implementing_tapestry(
+        node_ID,
+        pid,
+        destinationList,
+        routing_tables,
+        node_table,
+        indexed_actors
+      ) do
     Enum.reduce(destinationList, [], fn dest_ID, hop_list ->
-      hop_list ++ [next_hop(node_ID, dest_ID, routing_tables, node_table, 1)]
+      hop_list ++
+        [
+          GenServer.call(
+            pid,
+            {:UpdateNextHop, node_ID, dest_ID, routing_tables, node_table, 1, indexed_actors}
+          )
+        ]
     end)
   end
 
-  def next_hop(node_ID, dest_ID, routing_tables, node_table, total_hops) do
-    key = commonPrefix(node_ID, dest_ID)
-
-    if(Map.fetch!(node_table, key) == dest_ID) do
-      total_hops
-    else
-      new_node_ID = Map.fetch!(node_table, key)
-      new_node_table = Map.fetch!(routing_tables, new_node_ID)
-      next_hop(new_node_ID, dest_ID, routing_tables, new_node_table, total_hops + 1)
-    end
+  def nextHop(new_node_ID, dest_ID, routing_tables, new_node_table, total_hops, indexed_actors) do
+    GenServer.call(
+      Map.fetch!(indexed_actors, new_node_ID),
+      {:UpdateNextHop, new_node_ID, dest_ID, routing_tables, new_node_table, total_hops,
+       indexed_actors}
+    )
   end
 
   def fill_routing_table(hash_key, list_of_neighbors) do
@@ -116,7 +126,7 @@ defmodule Proj3 do
   end
 
   def init(:ok) do
-    {:ok, {0, 0, [], 1}}
+    {:ok, {0, 1}}
   end
 
   def start_node() do
@@ -128,10 +138,38 @@ defmodule Proj3 do
     GenServer.call(pid, {:UpdatePIDState, nodeID})
   end
 
+  def handle_call(
+        {:UpdateNextHop, node_ID, dest_ID, routing_tables, node_table, total_hops,
+         indexed_actors},
+        _from,
+        state
+      ) do
+    key = commonPrefix(node_ID, dest_ID)
+
+    if(Map.fetch!(node_table, key) == dest_ID) do
+      {:reply, total_hops, state}
+    else
+      new_node_ID = Map.fetch!(node_table, key)
+      new_node_table = Map.fetch!(routing_tables, new_node_ID)
+
+      final_hop_count =
+        nextHop(
+          new_node_ID,
+          dest_ID,
+          routing_tables,
+          new_node_table,
+          total_hops + 1,
+          indexed_actors
+        )
+
+      {:reply, final_hop_count, state}
+    end
+  end
+
   # Handle call for associating specific Node with PID
   def handle_call({:UpdatePIDState, nodeID}, _from, state) do
-    {a, b, c, d} = state
-    state = {nodeID, b, c, d}
+    {a, b} = state
+    state = {nodeID, b}
     {:reply, a, state}
   end
 end
